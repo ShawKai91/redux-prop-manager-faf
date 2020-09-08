a81082829ceb78111092a315e968d70838f35b8e
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-09-08 11:44:01 -0600

    setup & headerwrapper

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,19 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
-import App from "./components/app";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 import "./style/main.scss";
 
+import HeaderWraper from './components/headerWrapper';
+import Signup from './components/auth/signup';
+import Signin from './components/auth/signin';
+
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-        <App />
+      <Switch>
+        <HeaderWraper>
+          <Route path='/' exact component={Signin} />
+          <Route path='/signup' exact component={Signup} />
+        </HeaderWraper>
+      </Switch>
       </BrowserRouter>
     </Provider>,
     document.querySelector(".app-wrapper")
