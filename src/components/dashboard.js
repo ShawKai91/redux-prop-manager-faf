9bf9b7c6b85b670aededae266fae26c938bda1e4
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-10-07 13:55:32 -0600

    finished require auth and created browser history manually

---
+++ src/components/dashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div className='dashboard'>Hey There</div>
+        );
+    }
+}
+
+export default Dashboard;
