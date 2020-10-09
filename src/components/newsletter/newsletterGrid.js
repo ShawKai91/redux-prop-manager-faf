18b0e570146f783217b0ad76b8d056370c7a482a
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-10-09 13:49:20 -0600

    built newsletterbox and functionality

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+import NewsletterBox from './newsletterBox';
+
 class NewsletterGrid extends Component {
     render() {
         return (
             <div className='newsletter-grid'>
-                this is the newsletter content
+                {/* + btn */}
+                <NewsletterBox date={new Date()}/>
+                {/* arch */}
+                {/* content */}
             </div>
         )
     }
