64c932a13b34a993ebe2180384e0cea2314eafc9
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-10-09 13:02:03 -0600

    built the the newsletter grid component and grid

---
+++ src/components/newsletter/newsletterGrid.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewsletterGrid extends Component {
+    render() {
+        return (
+            <div className='newsletter-grid'>
+                this is the newsletter content
+            </div>
+        )
+    }
+}
+
+export default NewsletterGrid; 
