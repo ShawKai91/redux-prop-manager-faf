7a5f53a745a23d6428ee80554eca57d72297e8d8
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-10-13 14:57:53 -0600

    built requests reducer and change selected request type action creator

--- src/components/requests/requestsBox.js
+++ src/components/requests/requestsBox.js
@@ -4,7 +4,7 @@ class RequestsBox extends Component {
     render() {
         const { count, title } = this.props;
         return (
-            <a className='requests-box requests-box-inactive' onClick={() => console.log('tryna select this box')}>
+            <a className='requests-box requests-box-inactive' onClick={(title) => this.props.changeSelectedRequestType(title)}>
                 <div className='requests-box__count'>{count}</div>
                 <div className='requests-box__title'>{title}</div>
                 <div className='requests-box__point'></div>
