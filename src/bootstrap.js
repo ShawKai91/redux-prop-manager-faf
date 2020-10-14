1fc898967e30a48b6f63d4a7b34dd779652cf4cb
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-10-14 12:49:42 -0600

    +new request component

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -25,6 +25,8 @@ import NewNewsletter from './components/newsletter/newsletterNew';
 import EditNewsletter from './components/newsletter/newsletterEdit';
 import NewsletterDetail from './components/newsletter/newsletterDetail';
 
+import NewRequest from './components/requests/requestsNew';
+
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
@@ -42,7 +44,7 @@ function main() {
             <Route path='/newsletter/edit/:id' component={requireAuth(EditNewsletter)}/>
             <Route path='/newsletter/detail/:id' component={requireAuth(NewsletterDetail)}/>
 
-            <Route path='/request/new' component={NewNewsletter}/>
+            <Route path='/request/new' component={NewRequest}/>
             {/* <Route path='/request/new' component={requireAuth(NewNewsletter)}/> */}
           </Layout>
         </Switch>
