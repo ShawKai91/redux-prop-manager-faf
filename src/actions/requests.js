38fdb7e665cb2d9fb4bb40306fd304f89a542f75
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-10-14 13:12:33 -0600

    +create new request action creator with redux-thunk middleware

--- src/actions/requests.js
+++ src/actions/requests.js
@@ -1,12 +1,28 @@
 import {
     CHANGE_SELECTED_REQUEST_TYPE
    } from './types';
+
+import axios from 'axios';
+import { ROOT_URL } from '../config'; 
    
-   export function changeSelectedRequestType(boxType) {
-       return (
-           {
-               type: CHANGE_SELECTED_REQUEST_TYPE,
-               payload: boxType
-           }
-       )
-   }; 
+export function changeSelectedRequestType(boxType) {
+    return (
+        {
+            type: CHANGE_SELECTED_REQUEST_TYPE,
+            payload: boxType
+        }
+    )
+}; 
+
+export function createNewRequest(newRequest, success) {
+    return function() {
+        axios.post(`${ROOT_URL}/requests/new`, newRequest)
+            then(response => {
+                console.log(response.data);
+                success();
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+} 
